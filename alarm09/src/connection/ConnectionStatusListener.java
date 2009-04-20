081d8b4b8764072003a22f4b822c96c8751f57f5
Jan Berge Ommedal <jan.ommedal@gmail.com>
2009-04-20 19:48:46 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@46 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- alarm09/src/connection/ConnectionStatusListener.java
+++
@@ -1,9 +0,0 @@
-package connection;
-
-import connection.ConnectionStatusWrapper.ConnectionStatus;
-
-public interface ConnectionStatusListener {
-	
-	public void connectionStatusChanged(ConnectionStatus newStatus);
-
-}
