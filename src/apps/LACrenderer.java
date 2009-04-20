0cd04093ee538c7ba983bb60e319a75bc08b1de5
Jan Berge Ommedal <jan.ommedal@gmail.com>
2009-04-20 12:18:06 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@33 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- src/apps/LACrenderer.java
+++ src/apps/LACrenderer.java
@@ -3,46 +3,51 @@ package apps;
 import java.awt.Component;
 import java.awt.Insets;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
+
+import model.Sensor;
 
 /**
  * Klasse som genererer komponentente til elementene i listen i LACvinduet (sensorene)
  * @author Olannon
  *
  */
-public class LACrenderer extends JPanel implements ListCellRenderer, Values {
+public class LACrenderer extends DefaultListCellRenderer implements ListCellRenderer, Values {
 
 	@Override
-	public Component getListCellRendererComponent(JList arg0, Object arg1,
-			int arg2, boolean arg3, boolean arg4) {
+	public Component getListCellRendererComponent(JList list, Object object, int arg2, boolean arg3, boolean arg4) {
+		JLabel comp = (JLabel)super.getListCellRendererComponent(list, object, arg2, arg3, arg4);
 		
 		/*
 		 * Initialiserer komponenten som skal returneres som et panel med riktige mål
 		 */
-		JPanel panel = new JPanel();
-		panel.setBounds(LEFT_SPACE, TOP_SPACE + LABEL_HEIGHT + BUTTON_HEIGHT + 3*DEFAULT_SPACE, LIST_ELEMENT_WIDTH, LIST_ELEMENT_HEIGHT);
+		
+		
+		//JPanel panel = new JPanel();
+		//panel.setBounds(LEFT_SPACE, TOP_SPACE + LABEL_HEIGHT + BUTTON_HEIGHT + 3*DEFAULT_SPACE, LIST_ELEMENT_WIDTH, LIST_ELEMENT_HEIGHT);
+		
+		Sensor s = (Sensor)object;
 		
 		/*
 		 * Initialiserer og legger til de JLabelene som skal være i JPanelet
 		 */
-		JLabel ID = new JLabel("ID");
-		JLabel ROOMno = new JLabel("Room-number");
-		JLabel ROOMty = new JLabel("Room-type");
-		JLabel ROOMna = new JLabel("Room name");
-		JLabel SensorStatus = new JLabel("Sensor Status");
-		JLabel BatteryStatus = new JLabel("Battery Status");
-
-		panel.add(ID);
-		panel.add(ROOMno);
-		panel.add(ROOMty);
-		panel.add(ROOMna);
-		panel.add(SensorStatus);
-		panel.add(BatteryStatus);
+		JLabel ROOMno = new JLabel(s.getRoom().getRomInfo());
+		JLabel ROOMty = new JLabel(s.getRoom().getRomType());
+		JLabel ROOMna = new JLabel(""+s.getRoom().getRomNR());
+		JLabel SensorStatus = new JLabel((s.isAlarmState() ? "ALARM!" : "NO ALARM"));
+		JLabel BatteryStatus = new JLabel(""+s.getBattery());
 		
-		return panel;
+		comp.add(ROOMno);
+		comp.add(ROOMty);
+		comp.add(ROOMna);
+		comp.add(SensorStatus);
+		comp.add(BatteryStatus);
+		
+		return comp;
 		
 	}
 
