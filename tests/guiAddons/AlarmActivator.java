f4b648a2231d51d76a248dcf6111ddf6729eee9b
Eirik Andreas Stene <eirikandreas@gmail.com>
2009-04-22 11:31:54 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@88 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- tests/guiAddons/AlarmActivator.java
+++ tests/guiAddons/AlarmActivator.java
@@ -1,7 +1,17 @@
 package guiAddons;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import gui.BlinkingList;
+import gui.LACrenderer;
+import gui.Values;
 import help.AlarmHelp;
 import apps.LAC;
 import apps.MAC;
@@ -21,11 +31,59 @@ import model.Model;
  */
 
 
-public class AlarmActivator extends JFrame{
+public class AlarmActivator extends JFrame implements Values, ActionListener{
 
+	private JButton checkAlarm;
+	private JLabel sensors;
+	private JList sensorList;
+	private Model model;
+	private JLabel status;
+	private JLabel statusFelt;
 	
-	public AlarmActivator(Model m) {
-		// TODO Auto-generated constructor stub
+	
+	
+	public AlarmActivator(Model model) {
+		this.model = model;
+		
+	}
+	
+	public AlarmActivator(){
+		JPanel pane = new JPanel();
+		JFrame frame = new JFrame("Alarm check");
+		frame.setSize(700, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(pane);
+		frame.setVisible(true);
+		
+		
+		checkAlarm = new JButton("Check alarm");
+		checkAlarm.addActionListener(this);
+		
+		sensors = new JLabel("Sensors");
+		status = new JLabel("Status");
+		
+		pane.setLayout(null);
+		pane.add(sensors);
+		pane.add(status);
+		pane.add(statusFelt);
+		
+	JLabel[] statusf = new JLabel[20];
+		
+	for (int i = 0; i < statusf.length; i++) {
+			statusf[i] = new JLabel();
+			statusf[i].setBounds(100, 40+(i*10), 30, 30);
+	}
+		
+		
+	sensorList = new BlinkingList();
+	
+	sensorList.setCellRenderer(new LACrenderer());
+	sensorList.setVisibleRowCount(40); 
+	sensorList.setVisible(true);
+	pane.add(sensorList);
+	sensorList.setBounds(LEFT_SPACE, TOP_SPACE + BUTTON_HEIGHT + 3*DEFAULT_SPACE + LABEL_HEIGHT, LIST_WIDTH, LIST_HEIGHT);
+	sensorList.setFixedCellWidth(LIST_ELEMENT_WIDTH);
+	sensorList.setFixedCellHeight(LIST_ELEMENT_HEIGHT);
 	}
 	
 	
@@ -35,6 +93,10 @@ public class AlarmActivator extends JFrame{
 		Model m = AlarmHelp.getDefaultModel();
 		lac.setModel(m);
 		new AlarmActivator(m);
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
 		
 	}
 }
