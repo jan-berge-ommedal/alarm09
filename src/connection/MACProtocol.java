dbe62b2b68f13931c868ca3189817cad273620bd
Simon Grøndahl <gnomis@gmail.com>
2009-04-27 13:36:29 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@238 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- src/connection/MACProtocol.java
+++ src/connection/MACProtocol.java
@@ -36,15 +36,44 @@ public class MACProtocol {
 				else if(receive.substring(0, 9).equals("UPDATELAC")){
 					String[] s = receive.split(" ");
 					adaper.getMAC().getDatabase().updateLAC(Integer.parseInt(s[1]), s[2]);
+					adaper.getModel().setID(Integer.parseInt(s[1]));
+					adaper.getModel().setAdresse(s[2]);
 				}
 				else if(receive.substring(0, 10).equals("UPDATEROOM")){
 					String[] s = receive.split(" ");
 					adaper.getMAC().getDatabase().updateRoom(Integer.parseInt(s[1]), Integer.parseInt(s[2]), s[3], s[4]);
+					Room r = null;
+					for (Room room : adaper.getModel().getRooms()) {
+						if(room.getID() == Integer.parseInt(s[1])){
+							r = room;
+							break;
+						}
+					}
+					r.setRomNR(Integer.parseInt(s[2]));
+					r.setRomType(s[3]);
+					r.setRomInfo(s[4]);
 				}
 				else if(receive.substring(0, 12).equals("UPDATESENSOR")){
 					String[] s = receive.split(" ");
 					boolean b = (s[2].equals("true")) ? true : false;
 					adaper.getMAC().getDatabase().updateSensor(Integer.parseInt(s[1]), b, Integer.parseInt(s[3]), XmlSerializer.makeTimestamp(s[4]));
+					Room r = null;
+					for (Room room : adaper.getModel().getRooms()) {
+						if(room.getID() == Integer.parseInt(s[5])){
+							r = room;
+							break;
+						}
+					}
+					Sensor se = null;
+					for (Sensor sensor : r.getSensorer()) {
+						if(sensor.getId() == Integer.parseInt(s[1])){
+							se = sensor;
+							break;
+						}
+					}
+					se.setAlarmState(b);
+					se.setBattery(Integer.parseInt(s[3]));
+					se.setInstallationDate(XmlSerializer.makeTimestamp(s[4]));
 				}
 				else if(receive.substring(0, 10).equals("INSERTROOM")){
 					String[] s = receive.split(" ");
@@ -63,8 +92,20 @@ public class MACProtocol {
 				else if(receive.substring(0, 12).equals("INSERTSENSOR")){
 					String[] s = receive.split(" ");
 					boolean b = (s[2].equals("true")) ? true : false;
-					int sensorID = adaper.getMAC().getDatabase().insertSensor(Integer.parseInt(s[1]), b, Integer.parseInt(s[5]));
+					int romID = Integer.parseInt(s[1]);
+					int battery = Integer.parseInt(s[5]);
+					int sensorID = adaper.getMAC().getDatabase().insertSensor(romID, b, battery);
 					adaper.getConnection().send(Integer.toString(sensorID));
+
+					Room room = null;
+					for (Room r : adaper.getModel().getRooms()) {
+						if(r.getID() == romID){
+							room = r;
+						}
+					}
+					Timestamp time = XmlSerializer.makeTimestamp(s[3]);
+					
+					room.addSensor(new Sensor(romID, b, battery, time, room, true));
 				}
 				else if(receive.substring(0, 11).equals("INSERTEVENT")){
 					String[] s = receive.split(" ");
@@ -75,6 +116,23 @@ public class MACProtocol {
 					}
 					int eventID = adaper.getMAC().getDatabase().insertEvent(Integer.parseInt(s[1]), et);
 					adaper.getConnection().send(Integer.toString(eventID));
+					
+					Room r = null;
+					for (Room room : adaper.getModel().getRooms()) {
+						if(room.getID() == Integer.parseInt(s[3])){
+							r = room;
+							break;
+						}
+					}
+					Sensor se = null;
+					for (Sensor sensor : r.getSensorer()) {
+						if(sensor.getId() == Integer.parseInt(s[1])){
+							se = sensor;
+							break;
+						}
+					}
+					Timestamp time = new Timestamp(System.currentTimeMillis());
+					se.addEvent(new Event(eventID,et, time,se));
 				}
 				
 			} catch (Exception e) {
