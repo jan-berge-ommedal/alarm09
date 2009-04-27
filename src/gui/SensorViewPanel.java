06298fbbd176b5fbee41f5805c4a8e4fbf969cda
Jarle Svendsrund <jarle.svendsrud@outlook.com>
2009-04-27 10:29:04 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@212 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- src/gui/SensorViewPanel.java
+++ src/gui/SensorViewPanel.java
@@ -1,18 +1,42 @@
 package gui;
 
+import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 import model.Sensor;
 
-public class SensorViewPanel extends JPanel {
+public class SensorViewPanel {
 	
-	private Sensor sensor;
-	private JFrame frame;
-	
-	public SensorViewPanel(Sensor sensor) {
-		this.sensor = sensor;
-		frame = new JFrame("Sensor Events");
+	public static void viewSensorEvents(Sensor sensor) {
+		final JFrame frame = new JFrame("Sensor Events");
+		JPanel panel = new JPanel();
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(panel);
+		frame.setVisible(true);
+		JButton ok = new JButton("OK");
+		ok.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+			}
+			
+		}
+		);
+		Event[] events = new Event[sensor.getEvents().size()];
+		for (int i = 0; i < events.length; i++) {
+			//endre navn på eventklassen pliz, casting blir lol!
+		}
+		JList eventList = new JList(events);
+		panel.add(ok);
+		
 	}
 
 }
