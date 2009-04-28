e28d7427d0069e22af2a57ad75d8cdc1ab943b52
Eirik Andreas Stene <eirikandreas@gmail.com>
2009-04-28 09:20:48 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@246 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- tests/guiAddons/AlarmActivator.java
+++ tests/guiAddons/AlarmActivator.java
@@ -68,7 +68,6 @@ public class AlarmActivator extends JFrame implements Values{
 			JLabel label = new JLabel("Sensor "+s.getId());
 			JButton alarmButton = new JButton("Start alarm");
 			alarmButton.addActionListener(new ActionListener(){
-				@Override
 				public void actionPerformed(ActionEvent e) {
 					s.setAlarmState(null);
 				}
