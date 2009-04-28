f5a032052d9a8985758d651660e2ca51af10c45f
Andreas Stræte <a.straete@gmail.com>
2009-04-28 09:21:04 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@247 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- src/gui/SensorViewPanel.java
+++ src/gui/SensorViewPanel.java
@@ -2,16 +2,20 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Timestamp;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 
+import model.Model;
+import model.Room;
 import model.Sensor;
 
 public class SensorViewPanel implements Values {
-	
+
 	/**
 	 * Statisk metode som genererer et vindu med sensorlogg - dvs en liste av events
 	 * @param sensor
@@ -20,17 +24,19 @@ public class SensorViewPanel implements Values {
 		final JFrame frame = new JFrame("Sensor Events");
 		JPanel panel = new JPanel();
 		frame.setSize(500, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setContentPane(panel);
 		frame.setVisible(true);
-		JButton ok = new JButton("OK");
-		ok.addActionListener(new ActionListener() {
+		JButton close = new JButton("Close");
+		JButton checkSensor = new JButton("Check Sensor");
+		JButton alarm = new JButton("Stop Alarm");
+		close.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				frame.setVisible(false);
 			}
-			
+
 		}
 		);
 		JList eventList = new JList();
@@ -45,22 +51,43 @@ public class SensorViewPanel implements Values {
 			System.err.println("Sensor var null, why?");
 		}
 		eventList.setBounds(LEFT_SPACE, TOP_SPACE, 400, 200);
-		ok.setBounds(LEFT_SPACE + 500/3, 300, BUTTON_WIDTH, BUTTON_HEIGHT);
-		ok.setVisible(true);
+		close.setBounds(LEFT_SPACE +300, 300, BUTTON_WIDTH, BUTTON_HEIGHT);
+		checkSensor.setBounds(LEFT_SPACE + 150, 300, BUTTON_WIDTH +25, BUTTON_HEIGHT);
+		alarm.setBounds(LEFT_SPACE, 300, BUTTON_WIDTH + 25, BUTTON_HEIGHT);
+
+		if(sensor != null) {
+			if(!sensor.isAlarmState()) {
+				alarm.setVisible(false);
+			}
+		}
+
 		eventList.setVisible(true);
 		panel.setLayout(null);
 		panel.add(eventList);
-		panel.add(ok);
-		frame.repaint();
+
+		panel.add(checkSensor);
+		panel.add(alarm);
+		panel.add(close);
 		
+		if(sensor == null) {
+			checkSensor.setVisible(false);
+			alarm.setVisible(false);
+			eventList.setVisible(false);
+			JLabel noSensor = new JLabel("No sensor loaded!");
+			noSensor.setBounds(LEFT_SPACE, TOP_SPACE, 400, 200);
+			panel.add(noSensor);
+		}
+		
+		frame.repaint();
+
 	}
-	
+
 	/**
 	 * main for testing av sensorviewpanel
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		viewSensorEvents(null);
+		viewSensorEvents(new Sensor(2, true, 50, new Timestamp(0), new Room(2, 32, "Hus", "Stort",  new Model()), true));
 	}
 
 }
