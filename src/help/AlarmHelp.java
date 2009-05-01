cfaf845bc9dfc8f60f3059096df78ce4673aa2c1
Andreas Stræte <a.straete@gmail.com>
2009-05-01 20:07:15 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@369 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- src/help/AlarmHelp.java
+++ src/help/AlarmHelp.java
@@ -4,6 +4,7 @@ import unitTests.XMLParsingTests.DefaultModelEditController;
 import model.Model;
 import model.Room;
 import model.Sensor;
+import model.Sensor.Alarm;
 import apps.LAC;
 
 
@@ -14,10 +15,10 @@ public class AlarmHelp {
 		Model m = new Model(controller,0);
 		m.setAdresse("Lidarende 1");
 		Room r = new Room(0,54,"BAD","Et fint bad",m);
-		new Sensor(0,false,70,LAC.getTime(),r);
-		new Sensor(1,true,20,LAC.getTime(),r);
+		new Sensor(0,Alarm.DEACTIVATED,70,LAC.getTime(),r);
+		new Sensor(1,Alarm.ACTIVATED,20,LAC.getTime(),r);
 		Room r2 = new Room(0,2,"Kjøkken","Storkjøkkenet i huset",m);
-		new Sensor(2,false,100,LAC.getTime(),r2);
+		new Sensor(2,Alarm.DEACTIVATED,100,LAC.getTime(),r2);
 		return m;
 	}
 
