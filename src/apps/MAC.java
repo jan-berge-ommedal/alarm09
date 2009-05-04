63c0e5e046416eefa232b6cd76278ccc35d577bd
Andreas Stræte <a.straete@gmail.com>
2009-05-04 17:50:36 +0000

    git-svn-id: http://alarm09.googlecode.com/svn/trunk@372 49441d3a-eeb1-11dd-901b-0b69d2339e2b

--- src/apps/MAC.java
+++ src/apps/MAC.java
@@ -343,7 +343,7 @@ public class MAC{
 					System.exit(0);
 				} catch (SocketTimeoutException e1) {
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					//e1.printStackTrace();
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
